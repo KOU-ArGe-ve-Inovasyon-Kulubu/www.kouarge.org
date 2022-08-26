ebe261214556f205b4ddb4808760aa71e32ba7b3
Abdullah Ali GÜN <aagun16@gmail.com>
2022-08-26 21:12:00 +0300

    türkçe classlar silindi

--- www.kouarge.org/Models/TablesDb/EtkinlikKatılımcıListesi.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace www.kouarge.org.Models.TablesDb
-{
-    public class EtkinlikKatilimciListesi
-    {
-        public int id { get; set; }
-        public int etkinlikId { get; set; }
-        public int userId { get; set; }
-    }
-}
